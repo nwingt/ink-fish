737caf949570d6766d1c95e90653f30b35d30e44
William Chong <me@williamchong.cloud>
2023-10-14 02:43:06 +0800

    ➕ Add express session

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,38 @@
 import http from 'http';
 import express, { Request, Response } from 'express';
 import { Server } from 'socket.io';
+import { Firestore } from '@google-cloud/firestore';
+import session from 'express-session';
+import { FirestoreStore } from '@google-cloud/connect-firestore';
+
+const {
+  SESSION_SECRET = 'my-secret',
+} = process.env;
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const sessionMiddleware = session({
+  store: new FirestoreStore({
+    dataset: new Firestore(),
+    kind: 'express-sessions',
+  }),
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+});
+app.use(sessionMiddleware);
+io.engine.use(sessionMiddleware);
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript Express!');
 });
 
-io.on('connection', () => {
-  console.log('a user connected');
+io.on('connection', (socket) => {
+  const sessionId = (socket.request as any).session.id;
+
+  console.log(`user ${sessionId} connected`);
 });
 
 const port = process.env.PORT || 3000;
