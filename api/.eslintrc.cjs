1f36a44b01b7be832bed1d0983f1ef71d6e58394
William Chong <me@williamchong.cloud>
2023-10-13 15:58:53 +0800

    🚨 Add eslint to API

---
+++ api/.eslintrc.cjs
@@ -0,0 +1,7 @@
+/* eslint-env node */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
