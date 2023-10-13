3c323f38cb949141509fe133a2bb0b62c14ec02d
William Chong <me@williamchong.cloud>
2023-10-13 16:13:00 +0800

    ➕ Add socket.io

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,21 @@
+import http from 'http';
 import express, { Request, Response } from 'express';
+import { Server } from 'socket.io';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript Express!');
 });
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('a user connected');
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
